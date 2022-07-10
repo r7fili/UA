e9a075ad91481d3329a8a38c92a7562f2a015a5f
Gonçalo Cunha <goncalo.cunha@ua.pt>
2022-07-10 19:20:29 +0100

    about

--- sem2/LI/FINAL/FINAL/server.py
+++ sem2/LI/FINAL/FINAL/server.py
@@ -17,4 +17,4 @@ def sum():
     return jsonify({'result': received['a'] + received['b']})
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', port=8080, debug=True)
+    app.run(host= '127.0.0.1', port=10014, debug=True)
